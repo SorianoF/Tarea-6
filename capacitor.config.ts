1ca747721c10a78ff68e861d07e258306a9282af
Mario Eduardo <sorianofloresme@gmail.com>
2025-10-27 21:33:09 -0700

    Change in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.e502c418df2442d6879206b2c64a4e13',
+  appId: 'app.toolbox.wonder',
   appName: 'toolbox-wonder',
-  webDir: 'dist',
-  server: {
-    url: 'https://e502c418-df24-42d6-8792-06b2c64a4e13.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  webDir: 'dist'
 };
 
 export default config;
